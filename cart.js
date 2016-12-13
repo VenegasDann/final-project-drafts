181e1824eebe3aa8476cdb2e054c83441a76ac33
Pom's Chicken <8dvenegas@stpatrick.org>
2016-12-13 10:51:59 -0600

    Final Day 2

---
+++ cart.js
@@ -0,0 +1,15 @@
+class Cart{
+  //What is the first part of every class? Type it below.
+constructor (il, iq{
+this.itemList = il;
+this.itemQuantity = iq;
+}
+  //Type the instance functions below this comment.
+addItem(i,q){
+
+  this.itemList.unshift(i);
+  this.itemQuantity:unshift(q);
+}
+totalCart(){
+  var total = 0;
+}
---
+++ item.js
@@ -0,0 +1,13 @@
+class Item{
+  //Fill in the item class below this comment.
+constructor (n,p,s){
+this.name=n
+this.price=p
+this.shipping=s
+
+}
+
+//Create your three test items below this comment.
+new Item dl("doll",15,3);
+new Item tc("toy car",8,2);
+new Item bbs("building blocks",10,3);
